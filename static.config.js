01dffe6bfa7960d45595e865fff29c4eb6c44141
Billie Thompson <133327+PurpleBooth@users.noreply.github.com>
2021-12-04 20:52:07 +0100

    fix: Remove relative dot that didn't work

--- static.config.js
+++ static.config.js
@@ -4,7 +4,6 @@ import path from 'path'
 // Typescript support in static.config.js is not yet supported, but is coming in a future update!
 
 export default {
-  basePath: "./",
   entry: path.join(__dirname, 'src', 'index.tsx'),
   getRoutes: async () => {
     return [ ]
