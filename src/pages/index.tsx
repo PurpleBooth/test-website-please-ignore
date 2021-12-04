be05d5c6fa8e9e2eb39f7f7154ef84830aeb114d
Billie Thompson <billie@billiecodes.com>
2021-12-04 20:31:14 +0100

    feat: basic template app

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default () => (
+  <div className={"flex justify-center my-10"}>
+    <div className={"hover:animate-spin m-2 p-2 text-center bg-white rounded ring-solid ring-indigo-100 ring-2"}>
+      <h1 className={"font-serif text-7xl"}>
+        Test Website
+      </h1>
+      <p className={"font-sans text"}>
+        Please ignore
+      </p>
+    </div>
+  </div>
+)
