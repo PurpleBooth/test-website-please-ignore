4074909c67666d59fa7f17c6c45f00f94e958623
Billie Thompson <billie@billiecodes.com>
2021-12-05 16:07:29 +0100

    fix: Remove spinning
    It's sort of obnoxious

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ const Index: FunctionComponent = () => {
 
   return (
     <div className={"flex justify-center my-10"}>
-      <div className={"hover:animate-spin m-2 p-2 text-center bg-white rounded ring-solid ring-indigo-100 ring-2"}>
+      <div className={"m-2 p-2 text-center bg-white rounded ring-solid ring-indigo-100 ring-2"}>
         <h1 className={"font-serif text-7xl"}>
           Test Website
         </h1>
