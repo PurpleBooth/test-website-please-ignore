bd442c223b83323befa942af097a43d0a4c2699b
Billie Thompson <billie@billiecodes.com>
2021-12-05 15:24:43 +0100

    feat: Load some data from IPFS

---
+++ src/hooks/use-ipfs-factory.ts
@@ -0,0 +1,64 @@
+import { create, IPFS } from 'ipfs-core'
+import { useEffect, useState } from 'react'
+
+let ipfs: null | IPFS = null
+
+/*
+ * A quick demo using React hooks to create an ipfs instance.
+ *
+ * Hooks are brand new at the time of writing, and this pattern
+ * is intended to show it is possible. I don't know if it is wise.
+ *
+ * Next steps would be to store the ipfs instance on the context
+ * so use-ipfs calls can grab it from there rather than expecting
+ * it to be passed in.
+ */
+
+type FactoryOptions = { commands: string[] }
+type WindowWithIpfs = Window & typeof globalThis & { ipfs?: { enable: (some: FactoryOptions) => IPFS } };
+
+export default function useIpfsFactory(options: FactoryOptions) {
+  const [isIpfsReady, setIpfsReady] = useState(Boolean(ipfs))
+  const [ipfsInitError, setIpfsInitError] = useState(null)
+
+  useEffect(() => {
+    // The fn to useEffect should not return anything other than a cleanup fn,
+    // So it cannot be marked async, which causes it to return a promise,
+    // Hence we delegate to a async fn rather than making the param an async fn.
+
+    startIpfs()
+    return function cleanup() {
+      if (ipfs && ipfs.stop) {
+        console.log('Stopping IPFS')
+        ipfs.stop().catch((err: unknown) => console.error(err))
+        ipfs = null
+        setIpfsReady(false)
+      }
+    }
+  }, [])
+
+  async function startIpfs() {
+    let browser: undefined | WindowWithIpfs = window
+
+    if (ipfs) {
+      console.log('IPFS already started')
+    } else if (browser && browser?.ipfs && browser?.ipfs?.enable) {
+      console.log('Found window.ipfs')
+      ipfs = await browser.ipfs.enable(options)
+    } else {
+      try {
+        console.time('IPFS Started')
+        ipfs = await create()
+        console.timeEnd('IPFS Started')
+      } catch (error) {
+        console.error('IPFS init error:', error)
+        ipfs = null
+        setIpfsInitError(error)
+      }
+    }
+
+    setIpfsReady(Boolean(ipfs))
+  }
+
+  return { ipfs, isIpfsReady, ipfsInitError }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { FunctionComponent, useMemo, useState } from 'react'
+import useIpfsFactory from "../hooks/use-ipfs-factory";
 
-export default () => (
-  <div className={"flex justify-center my-10"}>
-    <div className={"hover:animate-spin m-2 p-2 text-center bg-white rounded ring-solid ring-indigo-100 ring-2"}>
-      <h1 className={"font-serif text-7xl"}>
-        Test Website
-      </h1>
-      <p className={"font-sans text"}>
-        Please ignore
-      </p>
+const Index: FunctionComponent = () => {
+  const { ipfs } = useIpfsFactory({ commands: ['cat'] })
+  const [text, setText] = useState<string | null>(null)
+  const ipfsPath: string = "QmVNZKviQ5iA4nyrgtofRwGY1umVnMiVTJfxJEnNHzJBZb";
+
+  useMemo(() => {
+    if (!ipfs) {
+      return;
+    }
+
+
+    (async () => {
+      const catResults = await ipfs.cat(ipfsPath)
+      let results = [];
+      for await (const x of catResults) {
+        results.push(new TextDecoder("utf8").decode(x))
+      }
+
+      setText(results.join(""))
+    })();
+  }, [ipfs, text, ipfsPath])
+
+  return (
+    <div className={"flex justify-center my-10"}>
+      <div className={"hover:animate-spin m-2 p-2 text-center bg-white rounded ring-solid ring-indigo-100 ring-2"}>
+        <h1 className={"font-serif text-7xl"}>
+          Test Website
+        </h1>
+        <p className={"font-sans text"}>
+          Please ignore
+        </p>
+
+        <p className={"font-sans text"}>test: {text}
+        </p>
+      </div>
     </div>
-  </div>
-)
+  );
+}
+
+export default Index
